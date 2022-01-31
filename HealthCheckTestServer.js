a27b6f00a19968faaff901cbdfb4773e01e80a85
Bielefeld <thejens90@gmail.com>
2022-01-31 14:08:39 +0100

    Added a small test script that acts as healthcheck server to test the healthcheck functionality

---
+++ HealthCheckTestServer.js
@@ -0,0 +1,22 @@
+//I use this script to test the healthcheck function of HanasuAI.
+//this file is a modifyed example server from ipc-healthcheck
+//you can find the example on how to use the server at https://github.com/BielefeldJ/ipc-healthcheck/blob/main/example-server.js 
+
+const HealthcheckServer = require('ipc-healthcheck/healthcheck-server');
+const healthcheckserver = new HealthcheckServer('twitchbots',100,1000 ,true);
+healthcheckserver.on('serviceCrashed', (name) => {
+	console.log('Service crashed: ' + name);
+});
+healthcheckserver.on('serviceError', (errmeg,service) => {
+	console.log(`Service ${service.name} with id ${service.id} send the following error: ${errmeg}`);
+});
+
+//Starts the HealthcheckServer
+healthcheckserver.startServer();
+
+process.on('SIGINT', () => {
+	//Stops the HealthcheckServer Server.
+	healthcheckserver.stopServer();
+	console.info('Shutdown example Server.');
+  });
+
